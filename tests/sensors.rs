2db9f8ec95d3e3659159659ca162f2e1202288eb
Rémy Raes <contact@remyraes.com>
2022-01-26 16:17:52 +0100

    [test] new sensor should be created

--- tests/sensors.rs
+++ tests/sensors.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod sensors_tests {
     use warp::hyper::body::HttpBody;
-    use wifi_tracking_api::structs::sensors::Sensors;
-    use wifi_tracking_api::controllers::sensors::get_all_sensors;
+    use wifi_tracking_api::structs::sensors::{Sensor, Sensors};
+    use wifi_tracking_api::controllers::sensors::{create_sensor, get_all_sensors};
     use warp::reply::Reply;
+    use wifi_tracking_api::structs::position::Position;
 
     #[tokio::test]
     async fn empty_sensors_at_instantiation() {
@@ -14,5 +15,26 @@ mod sensors_tests {
             .unwrap().into_response().into_body()
             .data().await.unwrap().unwrap();
         assert_eq!(bytes.len(), 2); // == []
+    }
+
+    #[tokio::test]
+    async fn create_new_sensor() {
+        let sensors = Sensors::new();
+        let test_sensor = Sensor {
+            id: "test_sensor".to_string(),
+            pos: Position {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0
+            }
+        };
+
+        let result = create_sensor(test_sensor, sensors.clone()).await;
+        assert_eq!(result.unwrap().into_response().status(), 200);
+
+        let sensors_bytes = get_all_sensors(sensors).await.unwrap()
+            .into_response().into_body()
+            .data().await.unwrap().unwrap();
+        assert_eq!(sensors_bytes, "[\"test_sensor\"]");
     }
 }
