727b137a12a7aa263c153b01ff2063b579739015
Remy Raes <raes.remy@gmail.com>
2022-01-28 17:09:27 +0100

    [test] rewrite get_distance_with_no_sensors test with format! method

--- tests/distance.rs
+++ tests/distance.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod distance_tests {
     use warp::hyper::body::HttpBody;
+    use warp::hyper::StatusCode;
     use wifi_tracking_api::structs::sensors::{Sensor, Sensors};
     use warp::reply::Reply;
     use wifi_tracking_api::controllers::distance::get_device_pos;
@@ -11,13 +12,13 @@ mod distance_tests {
         let sensors = Sensors::new();
         let store = Store::new();
         let device_id = "9a:8f:99:87:0d:62".to_string();
-        let expected = "No data was reported for device 9a:8f:99:87:0d:62.";
+        let expected = format!("No data was reported for device {}.", device_id);
 
         let result = get_device_pos(sensors, store, device_id).await;
         assert_eq!(result.is_ok(), true);
 
         let response = result.unwrap().into_response();
-        assert_eq!(response.status(), 404);
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
         assert_eq!(response.into_body().data().await.unwrap().unwrap(), expected);
     }
 }
