4b2db7001e6f6cdbd3c7cd83acc79bb04a1a8ef7
Remy Raes <raes.remy@gmail.com>
2022-01-28 16:06:41 +0100

    [fix] all Datum attributes are public

--- src/structs/data.rs
+++ src/structs/data.rs
@@ -3,11 +3,11 @@ use std::collections::VecDeque;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Datum {
-    sensor_id: String,
+    pub sensor_id: String,
     pub sender_mac: String,
-    receiver_mac: String,
-    rssi: i8,
-    timestamp: u64
+    pub receiver_mac: String,
+    pub rssi: i8,
+    pub timestamp: u64
 }
 
 pub type Data = VecDeque<Datum>;
