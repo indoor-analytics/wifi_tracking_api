0a8a2d065140a3d57e42b1f62cd8ed2c691e2054
Remy Raes <raes.remy@gmail.com>
2022-01-28 17:31:37 +0100

    [fix] use Store.get_device_data in get_device_pos method

--- src/controllers/distance.rs
+++ src/controllers/distance.rs
@@ -1,16 +1,23 @@
 use warp::hyper::StatusCode;
+use warp::Reply;
 use warp::reply::with_status;
 use crate::structs::sensors::Sensors;
 use crate::structs::store::Store;
 
 pub async fn get_device_pos(
     _sensors: Sensors,
-    _store: Store,
+    store: Store,
     device_id: String
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(
-        with_status(
-            format!("No data was reported for device {}.", device_id),
-            StatusCode::NOT_FOUND)
-    )
+    let device_data = store.get_device_data(device_id.clone());
+    if device_data.len() == 0 {
+        return Ok(
+            with_status(
+                format!("No data was reported for device {}.", device_id.clone()),
+                StatusCode::NOT_FOUND
+            )
+                .into_response()
+        );
+    }
+    Ok(StatusCode::NOT_IMPLEMENTED.into_response())
 }
