2c72b419cc939515e7022e55d8d9dcfb53c33a60
Remy Raes <contact@remyraes.com>
2022-01-30 20:57:33 +0100

    [test] get_distance_with_no_data test creates a sensor

--- tests/distance.rs
+++ tests/distance.rs
@@ -3,13 +3,24 @@ mod distance_tests {
     use warp::hyper::body::HttpBody;
     use warp::hyper::StatusCode;
     use wifi_tracking_api::structs::sensors::{Sensor, Sensors};
+    use wifi_tracking_api::structs::position::Position;
     use warp::reply::Reply;
     use wifi_tracking_api::controllers::distance::get_device_pos;
+    use wifi_tracking_api::controllers::sensors::create_sensor;
     use wifi_tracking_api::structs::store::Store;
 
     #[tokio::test]
-    async fn get_distance_with_no_sensors() {
+    async fn get_distance_with_no_data() {
         let sensors = Sensors::new();
+        let sensor = Sensor {
+            id: "azdazd".to_string(),
+            pos: Position {
+                x: 1.0,
+                y: 2.0,
+                z: 3.0
+            }
+        };
+        create_sensor(sensor, sensors.clone()).await.ok();
         let store = Store::new();
         let device_id = "9a:8f:99:87:0d:62".to_string();
         let expected = format!("No data was reported for device {}.", device_id);
