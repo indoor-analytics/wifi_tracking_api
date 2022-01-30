5e45e47e508483b6c7e2ddee0098b7a631b42ab9
Remy Raes <contact@remyraes.com>
2022-01-30 21:54:57 +0100

    [test] get_device_pos should return 409 when no sensors have been registered

--- tests/distance.rs
+++ tests/distance.rs
@@ -32,4 +32,18 @@ mod distance_tests {
         assert_eq!(response.status(), StatusCode::NOT_FOUND);
         assert_eq!(response.into_body().data().await.unwrap().unwrap(), expected);
     }
+
+    #[tokio::test]
+    async fn get_distance_with_no_sensors() {
+        let sensors = Sensors::new();
+        let store = Store::new();
+        let device_id = "9a:8f:99:87:0d:62".to_string();
+
+        let result = get_device_pos(sensors, store, device_id).await;
+        assert_eq!(result.is_ok(), true);
+
+        let response = result.unwrap().into_response();
+        assert_eq!(response.status(), StatusCode::CONFLICT);
+        assert_eq!(response.into_body().data().await.unwrap().unwrap(), "No sensors have been registered yet.");
+    }
 }
